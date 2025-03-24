ff3054a7c1b28a957e76069d8f9efa3eb89ea10c
dongyudadiguo <3662728251@qq.com>
2025-03-24 15:57:59 +0800

    Update makedll.c

--- makedll.c
+++ makedll.c
@@ -6,14 +6,6 @@
     extern void ***funcs_ptr;
     extern void **basic_funcs_ptr;
     extern int *funcs_size_ptr;
-
-    extern void *bse;
-    extern void *crt;
-    extern int imp;
-    extern FILE *file;
-    extern void **funcs;
-    extern void *basic_funcs;
-    extern int funcs_size;
 #else
     #include <stdlib.h>
     #include <string.h>
@@ -31,14 +23,6 @@
     typedef int* (*get_funcs_size_ptr_func)();
     typedef void** (*get_basic_funcs_ptr_func)();
 
-    static get_bse_ptr_func get_bse_ptr = NULL;
-    static get_crt_ptr_func get_crt_ptr = NULL;
-    static get_imp_ptr_func get_imp_ptr = NULL;
-    static get_file_ptr_func get_file_ptr = NULL;
-    static get_funcs_ptr_func get_funcs_ptr = NULL;
-    static get_funcs_size_ptr_func get_funcs_size_ptr = NULL;
-    static get_basic_funcs_ptr_func get_basic_funcs_ptr = NULL;
-
     void **bse_ptr; 
     void **crt_ptr;
     int *imp_ptr;
@@ -47,43 +31,16 @@
     void **basic_funcs_ptr;
     int *funcs_size_ptr;
 
-    void *bse;
-    void *crt;
-    int imp;
-    FILE *file;
-    void **funcs;
-    void *basic_funcs;
-    int funcs_size;
-
-    void initialize_globals() {
-        bse_ptr = get_bse_ptr();
-        bse = *bse_ptr;
-        crt_ptr = get_crt_ptr();
-        crt = *crt_ptr;
-        imp_ptr = get_imp_ptr();
-        imp = *imp_ptr;
-        file_ptr = get_file_ptr();
-        file = *file_ptr;
-        funcs_ptr = get_funcs_ptr();
-        funcs = *funcs_ptr;
-        funcs_size_ptr = get_funcs_size_ptr();
-        funcs_size = *funcs_size_ptr;
-        basic_funcs_ptr = get_basic_funcs_ptr();
-        basic_funcs = *basic_funcs_ptr;
-    }
-
     __declspec(dllexport) void loadfuncs() {
         HMODULE hModule = GetModuleHandle(NULL);
         
-        get_bse_ptr = (get_bse_ptr_func)GetProcAddress(hModule, "vmc_get_bse_ptr");
-        get_crt_ptr = (get_crt_ptr_func)GetProcAddress(hModule, "vmc_get_crt_ptr");
-        get_imp_ptr = (get_imp_ptr_func)GetProcAddress(hModule, "vmc_get_imp_ptr");
-        get_file_ptr = (get_file_ptr_func)GetProcAddress(hModule, "vmc_get_file_ptr");
-        get_funcs_ptr = (get_funcs_ptr_func)GetProcAddress(hModule, "vmc_get_funcs_ptr");
-        get_funcs_size_ptr = (get_funcs_size_ptr_func)GetProcAddress(hModule, "vmc_get_funcs_size_ptr");
-        get_basic_funcs_ptr = (get_basic_funcs_ptr_func)GetProcAddress(hModule, "vmc_get_basic_funcs_ptr");
-
-        initialize_globals();
+        bse_ptr = ((get_bse_ptr_func)GetProcAddress(hModule, "vmc_get_bse_ptr"))();
+        crt_ptr = ((get_crt_ptr_func)GetProcAddress(hModule, "vmc_get_crt_ptr"))();
+        imp_ptr = ((get_imp_ptr_func)GetProcAddress(hModule, "vmc_get_imp_ptr"))();
+        file_ptr = ((get_file_ptr_func)GetProcAddress(hModule, "vmc_get_file_ptr"))();
+        funcs_ptr = ((get_funcs_ptr_func)GetProcAddress(hModule, "vmc_get_funcs_ptr"))();
+        funcs_size_ptr = ((get_funcs_size_ptr_func)GetProcAddress(hModule, "vmc_get_funcs_size_ptr"))();
+        basic_funcs_ptr = ((get_basic_funcs_ptr_func)GetProcAddress(hModule, "vmc_get_basic_funcs_ptr"))();
         
         *funcs_ptr = realloc(*funcs_ptr, (*funcs_size_ptr + new_funcs_size) * sizeof(void*));
         
